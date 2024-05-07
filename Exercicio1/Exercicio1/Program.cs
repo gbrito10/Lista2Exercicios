d902ef41add0b78451f349aaae736bcb68d9d10e
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-05-07 00:16:40 -0300

    Add files via upload

---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.Write("Digite o primeiro valor: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            b = int.Parse(Console.ReadLine());
+
+            if (a > b)
+                Console.WriteLine("O primeiro valor é o maior");
+
+            else
+                if (b > a)
+                Console.WriteLine("O segundo valor é o maior");
+        }
+    }
+}
