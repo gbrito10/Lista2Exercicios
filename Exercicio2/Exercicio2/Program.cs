f999c8d14149f8008a4a56e672af4d169bfc6bbc
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-05-07 09:44:51 -0300

    Add files via upload

---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+
+            Console.Write("Digite o primeiro valor: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor: ");
+            b = int.Parse(Console.ReadLine());
+
+            if (a == b)
+                Console.WriteLine("Os dois valores são iguais");
+
+            else
+                if (a > b)
+                Console.WriteLine("O primeiro valor é maior");
+
+            else
+                Console.WriteLine("O segundo valor é maior");
+        }
+    }
+}
