ae1e161d11461e9da01ffc71a9663d589aad2466
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-05-07 15:54:13 -0300

    Add files via upload

---
+++ Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int area;
+
+            Console.Write("Digite a base do retângulo: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura do retângulo: ");
+            b = int.Parse(Console.ReadLine());
+
+            area = (a * b) / 2;
+
+            Console.Write("A área do retângulo é: ");
+            Console.WriteLine(area);
+
+            if (area > 100)
+                Console.WriteLine("Terreno grande");
+
+            else
+                if (area < 100)
+                Console.WriteLine("Terreno pequeno");
+        }
+    }
+}
