b667195cde61de5512922c3d4c6d825ab066b11c
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-05-07 16:19:24 -0300

    Add files via upload

---
+++ Exercicio6/Exercicio6/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int p;
+            int h;
+            double r;
+
+            Console.Write("Digite o peso do paciente: ");
+            p = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura do paciente em centímetros: ");
+            h = int.Parse(Console.ReadLine());
+
+            r = Math.Pow(h, 2) / p;
+
+            if (r < 20)
+                Console.WriteLine("O paciente está abaixo do peso ideal");
+
+            else
+            if (r >= 20 && r < 25)
+                Console.WriteLine("O paciente está com o peso ideal");
+
+            else
+            if (r >= 25) 
+            Console.WriteLine("O paciente está acima do peso");
+
+
+
+
+
+        }
+    }
+}
