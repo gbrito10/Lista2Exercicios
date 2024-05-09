9dc48b31d915fb4fca7a2f8ae2cab8c7ac013c6b
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-05-09 10:32:51 -0300

    Add files via upload

---
+++ Exercicio10/Exercicio10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double media;
+
+            Console.Write("Digite a primeira nota do aluno: ");
+            a = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a segunda nota do aluno: ");
+            b = double.Parse(Console.ReadLine());
+
+            media = (a + 2 * b) / 3;
+            Console.WriteLine("A média do aluno é: ");
+            Console.WriteLine(media);
+
+            if (media > 5)
+                Console.WriteLine("Aprovado");
+
+            else
+                Console.WriteLine("Reprovado");
+
+
+        }
+    }
+}
